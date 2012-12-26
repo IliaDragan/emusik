c898d800d9362ea8be88760da379bfab8918465e
Anatoly Muntean <am@inlead.dk>
2012-12-26 12:03:02 +0200

    case 1939 - Templates and makrup generation.

---
+++ modules/emusik_playlists/templates/playlist_item.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Single playlist item template.
+ *
+ * Available variables:
+ *   $title
+ *   $artist
+ *   $spotify
+ *   $wimp
+ *   $bibzoom
+ */
+?>
+<div class="emusik-playlist-item">
+  <!-- @todo -->
+  <!-- Add some FE display -->
+</div>
