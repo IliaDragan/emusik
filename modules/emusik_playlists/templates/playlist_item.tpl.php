85497fc2eb085d6d2e9001b0df98f045e61f7ce6
Anatoly Muntean <am@inlead.dk>
2013-01-23 16:23:20 +0200

    case 2110 - Bibiliotek and youtube links for the playlist.

--- modules/emusik_playlists/templates/playlist_item.tpl.php
+++ modules/emusik_playlists/templates/playlist_item.tpl.php
@@ -22,6 +22,8 @@
     print !empty($bibzoom) ? l('', $bibzoom, array('attributes' => array('class' => array('bibzoom'), 'target'=>'_blank'), 'absolute' => TRUE)) : '';
     print !empty($wimp) ? l('', $wimp, array('attributes' => array('class' => array('wimp'), 'target'=>'_blank'), 'absolute' => TRUE)) : '';
     print !empty($spotify) ? l('', $spotify, array('attributes' => array('class' => array('spotify'), 'target'=>'_blank'), 'absolute' => TRUE)) : '';
+    print !empty($bibliotek) ? l('', $bibliotek, array('attributes' => array('class' => array('bibliotek'), 'target'=>'_blank'), 'absolute' => TRUE)) : '';
+    print !empty($youtube) ? l('', $youtube, array('attributes' => array('class' => array('youtube'), 'target'=>'_blank'), 'absolute' => TRUE)) : '';
     ?>
   </td>
 </tr>
